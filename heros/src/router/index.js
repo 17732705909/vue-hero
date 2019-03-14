41d6e8f7e4833f03c82ce83f6a53ef1504b1f973
zhanglei <418538746@qq.com>
2019-03-14 15:55:10 +0800

    Create index.js

---
+++ heros/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+// 引入子组件
+import herolist from '../components/herolist.vue'
+import tool from '../components/tool.vue'
+import coat from '../components/coat.vue'
+import add from '../components/list/add.vue'
+import edit from '../components/list/edit.vue'
+
+var router = new Router({
+  routes: [
+    { name:'herolist', path: '/herolist', component: herolist },
+    { name:'tool', path: '/tool', component: tool },
+    { name:'coat', path: '/coat', component: coat },
+    { name:'add', path: '/add', component: add },
+    { name:'edit', path: '/edit/:id', component: edit }
+  ]
+})
+export default router
