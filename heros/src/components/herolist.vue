0f096ba22073d6f3acb16f7732d9982378fa60da
zhanglei <418538746@qq.com>
2019-03-14 21:45:40 +0800

    Update herolist.vue

--- heros/src/components/herolist.vue
+++ heros/src/components/herolist.vue
@@ -14,13 +14,13 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(item, i) in list" :key="i">
-                        <td>id</td>
-                        <td>姓名</td>
-                        <td>性别</td>
+                    <tr v-for="item in list" :key="item.id">
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.gender }}</td>
                         <td>
-                            <router-link to="/edit/" + item.id>修改</router-link>
-                            <a href="#">删除</a>
+                            <router-link :to="'/edit/' + item.id">修改</router-link>
+                            <a href="#" @click.prevent="del(item.id)">删除</a>
                         </td>
                     </tr>
                 </tbody>
@@ -41,7 +41,28 @@ export default {
         }
     },
     methods: {
-
+        getList() {
+            this.$http.get('/heroes')
+            .then(data => {
+                var { status, data } = data
+                if(status === 200) {
+                    this.list = data
+                }
+            })
+        },
+        del(id) {
+            this.$http.delete(`/heroes/${id}`)
+            .then(data => {
+                var {status} = data
+                if(status === 200) {
+                    console.log('ok')
+                }
+                this.getList()
+            })
+        }
+    },
+    mounted(){
+        this.getList()
     }
 }
 </script>
