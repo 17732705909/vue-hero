012ec97014db75b9b124c65e8fd572f733f65df2
zhanglei <418538746@qq.com>
2019-03-14 21:45:49 +0800

    Update add.vue

--- heros/src/components/list/add.vue
+++ heros/src/components/list/add.vue
@@ -4,30 +4,43 @@
         <form>
             <div class="form-group">
                 <label for="exampleInputEmail1">英雄名称</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="请输入英雄名称">
+                <input type="text" class="form-control" v-model="obj.name" id="exampleInputEmail1" placeholder="请输入英雄名称">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">英雄性别</label>
                 <div>
-                    <input type="radio" name="gender" value="男" />男&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input type="radio" name="gender" value="女" />女
+                    <input type="radio" v-model="obj.gender" name="gender" value="男" />男&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="radio" v-model="obj.gender" name="gender" value="女" />女
                 </div>
             </div>
-            <button type="submit" class="btn btn-success">新增</button>
+            <button type="submit" class="btn btn-success" @click="add()">新增</button>
         </form>
     </div>
 </template>
 
 <script>
-
+// import axios from 'axios'
 export default {
     data() {
         return {
-            
+            obj: {
+                name: '',
+                gender: '男'
+            }
         }
     },
     methods: {
-       
+        add() {
+            this.$http.post('/heroes', this.obj)
+            .then(data => {
+                var { status, data } = data
+                if(status === 201) {
+                    alert('添加成功!')
+                    this.obj.name = '',
+                    this.obj.gender = '男'
+                }
+            })
+        }
     }
 }
 </script>
