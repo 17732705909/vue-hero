a4ee9e40e0c387755aa9481132091fb9d733f2ae
zhanglei <418538746@qq.com>
2019-03-14 21:45:55 +0800

    Update edit.vue

--- heros/src/components/list/edit.vue
+++ heros/src/components/list/edit.vue
@@ -4,16 +4,17 @@
         <form>
             <div class="form-group">
                 <label for="exampleInputEmail1">英雄名称</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="请输入英雄名称">
+                <input type="text" v-model="obj.name" class="form-control" id="exampleInputEmail1" placeholder="请输入英雄名称">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">英雄性别</label>
                 <div>
-                    <input type="radio" name="gender" value="男" />男&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <input type="radio" name="gender" value="女" />女
+                    {{$route.params.id}}
+                    <input type="radio" v-model="obj.gender" name="gender" value="男" />男&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="radio" v-model="obj.gender" name="gender" value="女" />女
                 </div>
             </div>
-            <button type="submit" class="btn btn-success">修改</button>
+            <button type="submit" @click.prevent="edit(obj)" class="btn btn-success">修改</button>
         </form>
     </div>
 </template>
@@ -23,9 +24,33 @@
 export default {
     data() {
         return {
+            obj: {},
+            id:''
         }
     },
     methods: {
+        getId() {
+            this.id = this.$route.params.id
+            this.$http.get(`/heroes/${this.id}`)
+            .then(data => {
+                var { status,data } = data
+                if(status === 200) {
+                    this.obj = data
+                }
+            })
+        },
+        edit(obj) {
+            this.$http.put(`/heroes/${this.id}`, this.obj)
+            .then(data => {
+                var { status,data } = data
+                if(status === 200) {
+                    window.location = '/herolist'
+                }
+            })
+        }
+    },
+    mounted() {
+        this.getId()
     }
 }
 </script>
