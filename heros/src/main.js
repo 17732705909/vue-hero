6e370191ec70e33b48cc73e11ec0121a8f399174
zhanglei <418538746@qq.com>
2019-03-14 21:46:00 +0800

    Update main.js

--- heros/src/main.js
+++ heros/src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入axios
+import axios from 'axios'
 // 引入bootstrap静态文件
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // 引入自定义样式
 import './assets/css/index.css'
 import './assets/css/common.css'
+
+axios.defaults.baseURL = 'http://localhost:3000'
+
+Vue.prototype.$http = axios
 
 new Vue({
   el: '#app',
